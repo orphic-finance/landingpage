714f42b8675db4b0c83c0fe0b67f45e9d1c9b906
Armand Morin <72650161+Armand-Morin@users.noreply.github.com>
2024-04-21 22:32:08 +0200

    update

--- script.js
+++ script.js
@@ -14,36 +14,3 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-
-import { GUI } from 'https://cdn.skypack.dev/dat.gui'
-
-const CONFIG = {
-  gradient: true,
-  animated: true,
-  highlight: 2,
-  spread: 1,
-  primary: '#ffffff',
-  secondary: '#606060',
-}
-
-const CTRL = new GUI()
-
-const UPDATE = () => {
-  for (const key of Object.keys(CONFIG)) {
-    document.documentElement.style.setProperty(`--${key}`, CONFIG[key])
-  }
-  document.documentElement.setAttribute('data-gradient', CONFIG.gradient)
-  document.documentElement.setAttribute('data-animate', CONFIG.animated)
-}
-
-const HIGHLIGHT_ONE = CTRL.addFolder('Highlight One')
-HIGHLIGHT_ONE.add(CONFIG, 'highlight', 0, 5, 1).name('Spread').onChange(UPDATE)
-HIGHLIGHT_ONE.addColor(CONFIG, 'primary').name('Color').onChange(UPDATE)
-HIGHLIGHT_ONE.add(CONFIG, 'gradient').name('Use gradient?').onChange(UPDATE)
-HIGHLIGHT_ONE.add(CONFIG, 'animated').name('Animate gradient?').onChange(UPDATE)
-
-const HIGHLIGHT_TWO = CTRL.addFolder('Highlight Two')
-HIGHLIGHT_TWO.add(CONFIG, 'spread', 0, 5, 1).name('Spread').onChange(UPDATE)
-HIGHLIGHT_TWO.addColor(CONFIG, 'secondary').name('Color').onChange(UPDATE)
-
-UPDATE()
