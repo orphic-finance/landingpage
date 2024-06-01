b7f6a203e07cf3f6946b92ce5129fc5701a81544
Armand Morin <72650161+Armand-Morin@users.noreply.github.com>
2024-06-02 01:17:38 +0200

    ze

--- script.js
+++ script.js
@@ -1,64 +1,3 @@
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function getRandomColor() {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
-function generateRandomGradient() {
-  const gradientTypes = ["circle at", "ellipse at"];
-  const gradientPositions = [
-    "10%",
-    "20%",
-    "30%",
-    "40%",
-    "50%",
-    "60%",
-    "70%",
-    "80%",
-    "90%",
-  ];
-  const gradientSizes = [
-    "10%",
-    "20%",
-    "30%",
-    "40%",
-    "50%",
-    "60%",
-    "70%",
-    "80%",
-    "90%",
-    "100%",
-  ];
-
-  const gradients = Array.from({ length: 3 }, () => {
-    const type = gradientTypes[getRandomInt(0, gradientTypes.length - 1)];
-    const xPos =
-      gradientPositions[getRandomInt(0, gradientPositions.length - 1)];
-    const yPos =
-      gradientPositions[getRandomInt(0, gradientPositions.length - 1)];
-    const startColor = getRandomColor();
-    const endColor = getRandomColor();
-    const startSize = gradientSizes[getRandomInt(0, gradientSizes.length - 1)];
-    const endSize = gradientSizes[getRandomInt(0, gradientSizes.length - 1)];
-    return `radial-gradient(${type} ${xPos} ${yPos}, ${startColor} ${startSize}, ${endColor} ${endSize})`;
-  });
-
-  return gradients.join(", ");
-}
-
-function applyRandomGradient() {
-  const gradientElement = document.querySelector(".gradient");
-  const gradient = generateRandomGradient();
-  gradientElement.style.background = gradient;
-}
-
 document.addEventListener("DOMContentLoaded", applyRandomGradient);
 
 const backgroundElement = document.querySelector(".background"); // Get the background element
@@ -158,5 +97,4 @@ function setRandomBackground() {
   backgroundElement.style.background = backgroundOptions[randomIndex];
 }
 
-// Call the function to set a random background on page load
 setRandomBackground();
